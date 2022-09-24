3a4f1b8229d29c08ca6172dc054206e6439869c8
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-09-24 16:32:20 +0700

    lab4

--- helloworld/greenwich.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "<h1>Welcome to Greenwich Vietnam !</h1>";
--- helloworld/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<h1 style='color: red';>Hello World</h1>";
-echo "<h2 style='color: blue';>Deploy code to Heroku successfully ! </h2>";
