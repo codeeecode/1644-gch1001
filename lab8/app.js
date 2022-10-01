13ff55f584fa02b0a0016a6449252f5671129e9c
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-10-01 17:20:51 +0700

    web

--- lab8/app.js
+++ lab8/app.js
@@ -1,10 +1,15 @@
+//module import
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 
+//database connect + access
 const mongoose = require("mongoose");
 const studentModel = require("./models/StudentSchema");
+//url connection string (local db)
+//const url = "mongodb://localhost:27017/greenwich";
+//url connection string (cloud db)
 const url =
   "mongodb+srv://longndt:xdHB48B3Fv*%40TPa@cluster0.i5kings.mongodb.net/greenwich";
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
@@ -13,6 +18,38 @@ mongoose.connect(url, { useNewUrlParser: true }, (err) => {
   } else {
     console.log("connect to db succeed !");
   }
+});
+
+//body-parser: get form input
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//render ra form add
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+//nhận và xử lý dữ liệu từ form add
+app.post("/add", (req, res) => {
+  //console.log(req.body);
+  //res.send(req.body);
+  //res.render("output", { student: req.body });
+
+  //tạo object student chứa dữ liệu nhập từ form
+  var student = new studentModel({
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+    image: req.body.image,
+  });
+  //lưu object student vào database
+  student.save((err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.redirect("/student");
+    }
+  });
 });
 
 app.set("views", path.join(__dirname, "views"));
@@ -24,6 +61,21 @@ app.get("/", (req, res) => {
 
 app.get("/about", function (req, res) {
   res.render("about");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var check = "Login failed !";
+  if (username == "admin" && password == "123456") {
+    check = "Login succeed !";
+  }
+  //res.send(check);
+  res.render("check", { result: check });
 });
 
 app.get("/student", (req, res) => {
