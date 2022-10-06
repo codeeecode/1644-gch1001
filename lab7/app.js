19baf702b52e86a74acf23f3db4ba3ddc606cc8d
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-10-06 13:10:55 +0700

    ok

--- lab7/app.js
+++ lab7/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //cấu hình views & view engine
 app.set("views", path.join(__dirname, "/views"));
